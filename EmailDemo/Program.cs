089f6d2f15c576b41772c76f403d9c235cf39c2f
VictorMarri <victorpujoni@gmail.com>
2021-02-09 16:36:39 -0300

    Primeiro Commit

---
+++ EmailDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Smtp;
+using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sender = new SmtpSender(() => new SmtpClient(host: "localhost")
+            {
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+                PickupDirectoryLocation = @"C:\Demos"
+            });
+
+            Email.DefaultSender = sender;
+
+            var email = await Email
+                .From(emailAddress: "victormarri@exchange.com")
+                .To(emailAddress: "jorge@gmail.com")
+                .Subject(subject: "Coleee")
+                .Body(body: "Valeu pela ajuda")
+                .SendAsync();
+        }
+    }
+}
