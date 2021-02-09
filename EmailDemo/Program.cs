6c694b9e4085d26ae6a51ec461d2d0f11548f18b
VictorMarri <victorpujoni@gmail.com>
2021-02-09 16:45:15 -0300

    Descrições dos passos para ajudar

--- EmailDemo/Program.cs
+++ EmailDemo/Program.cs
@@ -12,19 +12,19 @@ namespace EmailDemo
         {
             var sender = new SmtpSender(() => new SmtpClient(host: "localhost")
             {
-                EnableSsl = false,
-                DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
-                PickupDirectoryLocation = @"C:\Demos"
+                EnableSsl = false, //Como estamos fazendo local, nao precisamos configurar segurança, por isso eu deixo ele em false
+                DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory, //Aqui estou dizendo que vou salvar em um diretorio os dados que chegarem desse envio
+                PickupDirectoryLocation = @"C:\Demos" //Aqui estou especificando onde vai ficar registrado/guardado, para fins de teste
             });
 
-            Email.DefaultSender = sender;
+            Email.DefaultSender = sender; //Aqui to setando quem vai estar mandando o email, e como eu setei em 'sender' partirá do meu localhost
 
             var email = await Email
-                .From(emailAddress: "victormarri@exchange.com")
-                .To(emailAddress: "jorge@gmail.com")
-                .Subject(subject: "Coleee")
-                .Body(body: "Valeu pela ajuda")
-                .SendAsync();
+                .From(emailAddress: "victormarri@exchange.com") //Quem ta enviando o email
+                .To(emailAddress: "jorge@gmail.com") //Para quem ta enviando o email
+                .Subject(subject: "Coleee") //O título do email
+                .Body(body: "Valeu pela ajuda") //O conteúdo do email
+                .SendAsync(); //Enviar o email
         }
     }
 }
